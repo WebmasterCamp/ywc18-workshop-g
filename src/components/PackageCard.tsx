67e2e82f8d8bbf4787f977329a8cd5b263d8ccca
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-08-14 13:31:16 +0700

    Add home banners

---
+++ src/components/PackageCard.tsx
@@ -0,0 +1,17 @@
+import "./PackageCard.scss";
+import React, { Component } from "react";
+
+const PackageCard: React.FC = () => {
+  return (
+    <div className="package-card">
+      <div className="upper">
+
+      </div>
+      <div className="lower">
+        
+      </div>
+    </div>
+  );
+};
+
+export default PackageCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CarouselComponent from "../components/Carousel/CarouselComponent";
 import InstructionCard from "../components/InstructionCard";
+import PackageCard from "../components/PackageCard";
+import ProductCard from "../components/ProductCard";
 import "./Home.scss";
 
 const mockSeasonalProduct = [
@@ -41,6 +43,21 @@ const mockInstructions = [
   }
 ];
 
+const mockTopProducts = [
+  {},
+  {},
+  {},
+  {},
+  {},
+  {}
+];
+
+const mockPackages = [
+  {},
+  {},
+  {}
+];
+
 const Home: React.FC = () => {
   const renderInstructions = () => {
     return mockInstructions.map((x, i) => {
@@ -50,19 +67,61 @@ const Home: React.FC = () => {
     });
   };
 
+  const renderProducts = () => {
+    return mockTopProducts.map((x, i) => {
+      return (
+        <ProductCard key={i} />
+      );
+    });
+  };
+
+  const renderPackages = () => {
+    return mockPackages.map((x, i) => {
+      return (
+        <PackageCard key={i} />
+      );
+    });
+  };
+
   return (
-    <main id="home">
-      <div className="margin-container">
-        <div className="sample-box"></div>
-        <div className="instruction-container">
-          {renderInstructions()}
+    <>
+      <main id="home">
+        <div className="margin-container">
+          <div className="sample-box"></div>
+          <div className="instruction-container">
+            {renderInstructions()}
+          </div>
+        </div>
+        <CarouselComponent 
+          title="Seasonal product"
+          viewAllLink="/viewAll"
+          cards={mockSeasonalProduct}
+        />
+        <div className="margin-container">
+          <div className="our-products-container">
+            <h3>Our Products</h3>
+            <div className="sml-grid">
+              {renderProducts()}
+            </div>
+            <div className="btn-container">
+              <button className="btn">View All</button>
+            </div>
+          </div>
+        </div>
+      </main>
+      <div className="join-our-community-container">
+        <div className="margin-container">
+          <aside>
+            <h1>Join Our Community</h1>
+            <div>Register to our innovative farmers.</div>
+            <button className="btn">Register</button>
+          </aside>
+          <aside>
+            
+          </aside>
         </div>
       </div>
-      <CarouselComponent 
-        title="Seasonal product"
-        viewAllLink="/viewAll"
-        cards={mockSeasonalProduct} />
-    </main>
+    </>
   );
 };
 
