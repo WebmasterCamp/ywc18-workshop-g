d935b088d0a0933ca6c77347d9d966a05d3b4de7
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-08-14 12:10:24 +0700

    Add instruction card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CarouselComponent from "../components/Carousel/CarouselComponent";
+import InstructionCard from "../components/InstructionCard";
 import "./Home.scss";
 
 const mockSeasonalProduct = [
@@ -25,11 +26,37 @@ const mockSeasonalProduct = [
   },
 ];
 
+const mockInstructions = [
+  {
+    text: "Explore Our Story"
+  },
+  {
+    text: "Click to Adopt"
+  },
+  {
+    text: "Wait to Grow"
+  },
+  {
+    text: "Recieve Your Fruit"
+  }
+];
+
 const Home: React.FC = () => {
+  const renderInstructions = () => {
+    return mockInstructions.map((x, i) => {
+      return (
+        <InstructionCard key={x.text} index={i + 1} text={x.text} />
+      );
+    });
+  };
+
   return (
     <main id="home">
       <div className="margin-container">
         <div className="sample-box"></div>
+        <div className="instruction-container">
+          {renderInstructions()}
+        </div>
       </div>
       <CarouselComponent 
         title="Seasonal product"
