ebc534ffbdaf113db894279cda8e68a49a98d9cb
asmbd <asmb.dwr@gmail.com>
2021-08-14 11:24:06 +0700

    [+] add: viewAll and product pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ViewAll from "./pages/ViewAll";
 
 const App: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact={true}>
         <Home />
+      </Route>
+      <Route path="/viewAll" exact={true}>
+        <ViewAll />
+      </Route>
+      <Route path="/products/:id" exact={true}>
+        <Product />
       </Route>
       <Route path="*">
         <div>404</div>
