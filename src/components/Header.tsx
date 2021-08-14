f48272d023f66c24deeeafbaafca4532d93df695
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-08-14 16:33:08 +0700

    Update home icons and images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,30 +23,42 @@ const Header: React.FC = () => {
       <header className="mobile">
         <div className="margin-wrapper">
           <aside>
-            <div className="pre-icon" onClick={expand}></div>
+            <img src="./images/hamburger.svg" alt="icon" onClick={expand} />
           </aside>
           <aside>
-            <div>logo</div>
+            <div className="img-container">
+              <img src="./images/logo.svg" alt="logo" width={32} height={32} />
+            </div>
+            <div>Seedtory</div>
           </aside>
           <aside>
-            <div>cart</div>
+            <div className="img-container">
+              <img src="./images/cart.svg" alt="icon" width={24} height={24} />
+            </div>
           </aside>
         </div>
       </header>
       <header className="desktop">
         <div className="margin-wrapper">
           <aside>
+            <div className="img-container">
+              <img src="./images/logo.svg" alt="logo" width={32} height={32} />
+            </div>
             <div>logo</div>
           </aside>
           <aside>
-            <div>cart</div>
+            <div className="img-container">
+              <img src="./images/cart.svg" alt="icon" width={24} height={24} />
+            </div>
           </aside>
         </div>
       </header>
       <nav id="sidenav" className={cx({ "expanded": state.expanded })}>
         <div className="content-container">
           <div className="nav-header">
-            <div className="pre-icon" onClick={expand}></div>
+            <div className="img-container">
+              <img src="./images/hamburger.svg" alt="icon" onClick={expand} width={32} height={32} />
+            </div>
           </div>
         </div>
       </nav>
