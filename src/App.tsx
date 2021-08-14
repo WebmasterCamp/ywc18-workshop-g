ac8c4916a2f37f3feb3d36949aacd2d07043ab0a
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-08-14 11:25:17 +0700

    Add responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React, { Component } from "react";
 import "./App.scss";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/" exact={true}>
-        <Home />
-      </Route>
-      <Route path="*">
-        <div>404</div>
-      </Route>
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+        <Route path="*">
+          <div>404</div>
+        </Route>
+      </Switch>
+    </>
   );
 };
 
